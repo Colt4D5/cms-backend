9a1d94c92b060c96ecb4d9c825582ed3843a7fcb
Colton <allen.colton@hotmail.com>
2022-11-12 15:37:30 -0800

    initial commit

---
+++ src/api/homepage-slide/routes/homepage-slide.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * homepage-slide router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::homepage-slide.homepage-slide');
---
+++ src/api/homepage-slide/services/homepage-slide.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * homepage-slide service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::homepage-slide.homepage-slide');
